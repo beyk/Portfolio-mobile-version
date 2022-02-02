a84777a7b508c556056699cb3971e02842a5e9be
beyk <beykmail@gmail.com>
2022-02-02 19:58:36 +0330

    Modify main.js linters errors

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
-const menuLink = document.querySelectorAll(".menu-Links");
+const menuLink = document.querySelectorAll('.menu-Links');
 
-
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+function show (){
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
 }
-function close(){
-    mainMenu.style.top = '-100%';
-    mainMenu.style.display = 'none';
+function close (){
+  mainMenu.style.top = '-100%';
+  mainMenu.style.display = 'none';
 }
 
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
+openMenu.addEventListener ('click', show);
+closeMenu.addEventListener ('click', close);
 
 for (let i = 0; i < menuLink.length; i += 1) {
-    menuLink[i].addEventListener('click', close);
+  menuLink[i].addEventListener ('click', close);
   }
