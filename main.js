cac1dd0c1c4db27e400e4eab48aee9d394120a8c
beyk <beykmail@gmail.com>
2022-02-03 10:25:03 +0330

    Modify Eslint Errors

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 const menuLink = document.querySelectorAll('.menu-Links');
 
-function show (){
+function show() {
   mainMenu.style.display = 'flex';
   mainMenu.style.top = '0';
 }
-function close (){
+function close() {
   mainMenu.style.top = '-100%';
   mainMenu.style.display = 'none';
 }
 
-openMenu.addEventListener ('click', show);
-closeMenu.addEventListener ('click', close);
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
 
 for (let i = 0; i < menuLink.length; i += 1) {
-  menuLink[i].addEventListener ('click', close);
-  }
+  menuLink[i].addEventListener('click', close);
+}
