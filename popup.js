c751f5559a78302bc42af3936e4cfe33fea19b59
beyk <beykmail@gmail.com>
2022-02-04 16:50:25 +0330

    Modify index.hmtl bugs

--- popup.js
+++ popup.js
@@ -96,11 +96,82 @@ for (let i = 0; i < data.length; i += 1) {
         
         <button type="button" id="open-me" class='see-proj see-proj-${i}'>See project</button>
       </div>
-    </div>`;
+    </div>
+			<section class="detail-section">
+      <div class="popup">
+        <div class="modal">
+        <div class="modal-div-1">
+          <h2 class="modal-title">x</h2>
+          <ion-icon class="close-modal" name="close-outline"></ion-icon>
+          <span> <a  href="#" class="close-popUp">X</a></span>
+        </div>
+        <div class="text-info">
+          <ul>
+            <li class="client-name">CANOPY</li>
+            <li class="role">Back End Dev</li>
+            <li class="year">2015</li>
+          </ul>
+        <div class="modal-div-3">
+          <div class="modal-img">
+          <img class="snapshoot1-mob" src="images/Snapshoot1.png" alt="snapshoot1" />
+          <img class="snapshoot1-desk" src="#" alt="snapshoot1" />
+          </div>
+          <div class="modal-mobile-detail">
+          <!-- <div> -->
+            <p class="modal-description desk mob">
+            Lorem Ipsum is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but
+            also the leap into electronic typesetting, remaining essent.
+            </p>
+            <p class="modal-description  ">
+            Lorem Ipsum is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it 1960s with the
+            releaLorem Ipsum is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it 1960s with the
+            releorem Ipsum is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum han printer took a galley of
+            type and scrambled it 1960s with the releawn printer took a
+            galley of type and scrambled it 1960s with the releaLorem
+            Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy
+            text ever since the 1500s, when an unknown printer took a
+            galley of type and scrambled it 1960s with the relea
+            </p>
+          <div class="tech">
+            <ul class="modal-tools mobil">
+            <li class="tool">html</li>
+            <li class="tool">css</li>
+            <li class="tool">javaScript</li>
+            </ul>
+            <ul class="modal-tools desk">
+            <li class="tool">github</li>
+            <li class="tool">ruby</li>
+            <li class="tool">Bootstrap</li>
+            </ul>
+            <button type="button" class="modal-btn">
+            See Live <i class="fa fa-external-link" aria-hidden="true"></i>
+            </button>
+            <button type="button" class="modal-btn">
+            See Source
+            <i class="fa fa-github-square" aria-hidden="true"></i>
+            </button>
+          </div>
+          </div>
+        </div>
+        </div>
+      </div>
+      </section>`;
 }
 
 document.addEventListener('click', (click) => {
-  if (click.target.id === 'open-me') {
+  if (click.target.id == 'open-me') {
     popUp.style.display = 'block';
     modal.style.display = 'block';
   }
